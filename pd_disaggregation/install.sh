fe9a2a7b32f7758e32e02b22275b22bdd20f5300
chifeichi <chifeichi@192.168.1.141>
2026-09-05 10:26:55 +0800

    feat(pd_disaggregation): update installation workflow

--- pd_disaggregation/install.sh
+++ pd_disaggregation/install.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 set -ex
-CANN_INSTALL_PATH=${CANN_INSTALL_PATH:-"/usr/local/Ascend"}
+
+apt-get update
+mkdir -p /var/lib/alternatives
+apt-get install mpich libmpich-dev -y
+
+CANN_INSTALL_PATH=${CANN_INSTALL_PATH:-"/mnt/share/t00986241/b106"}
 source ${CANN_INSTALL_PATH}/ascend-toolkit/set_env.sh
 source ${CANN_INSTALL_PATH}/nnal/atb/set_env.sh
 
 echo "1. install akernel and openyuanrong"
-pip install akernel_sdk-0.9.19-py3-none-any.whl && pip install openyuanrong_sdk==0.7.47
+pip install akernel_sdk-0.9.19-py3-none-any.whl
 
 echo "2. install vllm-ascend from source"
 git clone -b releases/v0.23.0 https://github.com/vllm-project/vllm-ascend.git
 cd vllm-ascend && pip install -r requirements.txt --extra-index-url https://triton-ascend.osinfra.cn/pypi/simple/ --trusted-host triton-ascend.osinfra.cn
 export COMPILE_CUSTOM_KERNELS=1
-pip install -v -e . --extra-index-url https://triton-ascend.osinfra.cn/pypi/simple/ --trusted-host triton-ascend.osinfra.cn 
+MAX_JOBS=128 pip install -v -e . --extra-index-url https://triton-ascend.osinfra.cn/pypi/simple/ --trusted-host triton-ascend.osinfra.cn 
 cd ..
 
 echo "3. install mooncake"
 git clone -b v0.3.9 --depth 1 https://github.com/kvcache-ai/Mooncake.git
 cd Mooncake
+echo 'check_certificate = off' >> /etc/wgetrc
 sed -i 's|https://go.dev/dl/|https://golang.google.cn/dl/|g' dependencies.sh
-apt-get install mpich libmpich-dev -y
+sed -i '249s#golang\.google\.cn/dl#mirrors.aliyun.com/golang#g' dependencies.sh
 bash dependencies.sh -y
 mkdir build
 cd build
 cmake .. -DUSE_ASCEND_DIRECT=ON -DPython3_EXECUTABLE="$(which python)"
-make -j
+make -j128
 make install
-export LD_LIBRARY_PATH=/usr/local/Ascend/ascend-toolkit/latest/python/site-packages/mooncake:$LD_LIBRARY_PATH
-cd ..
+cp mooncake-common/src/libmooncake_common.so ${CANN_INSTALL_PATH}/ascend-toolkit/latest/python/site-packages/mooncake
+cp mooncake-transfer-engine/src/libtransfer_engine.so ${CANN_INSTALL_PATH}/ascend-toolkit/latest/python/site-packages/mooncake
+cp mooncake-store/src/libmooncake_store.so ${CANN_INSTALL_PATH}/ascend-toolkit/latest/python/site-packages/mooncake
+export LD_LIBRARY_PATH=${CANN_INSTALL_PATH}/ascend-toolkit/latest/python/site-packages/mooncake:$LD_LIBRARY_PATH
+cd ../../
 
 echo "4.install verl"
 git clone https://github.com/verl-project/verl.git
@@ -42,4 +51,7 @@ git apply --whitespace=nowarn ../verl-ascend-recipe/pd_disaggregation/patch/vllm
 cd verl
 git apply --whitespace=nowarn ../verl-ascend-recipe/pd_disaggregation/patch/verl.patch && cd ..
 
-
+echo "5.run uni-agent"
+git clone https://github.com/chifeichi/uni-agent.git
+cd uni-agent && git checkout cfc/new_release
+bash examples/blackbox_recipes/claude_code/run_train_no_pd.sh
