d88d588eda4322001c897492b9290ab7b3ca1884
chifeichi <chifeichi@192.168.1.141>
2026-09-05 22:54:06 +0800

    fix(pd_disaggregation): restore missing dpkg helper

--- pd_disaggregation/install.sh
+++ pd_disaggregation/install.sh
@@ -6,6 +6,18 @@ mkdir -p /var/lib/alternatives
 if [ -f /etc/apt/apt.conf.d/70debconf ] && [ ! -x /usr/sbin/dpkg-preconfigure ]; then
     mv /etc/apt/apt.conf.d/70debconf /etc/apt/apt.conf.d/70debconf.disabled
 fi
+if [ ! -x /usr/sbin/start-stop-daemon ] && [ ! -x /sbin/start-stop-daemon ]; then
+    DPKG_REPAIR_DIR=$(mktemp -d)
+    (
+        cd "${DPKG_REPAIR_DIR}"
+        apt-get download dpkg
+        dpkg-deb -x ./dpkg_*.deb extracted
+        START_STOP_DAEMON=$(find extracted -type f -path '*/sbin/start-stop-daemon' -print -quit)
+        test -n "${START_STOP_DAEMON}"
+        install -D -m 0755 "${START_STOP_DAEMON}" /usr/sbin/start-stop-daemon
+    )
+    rm -rf "${DPKG_REPAIR_DIR}"
+fi
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH:-} \
     DEBIAN_FRONTEND=noninteractive apt-get install -y mpich libmpich-dev
 
