ee6d221cf5562de05c96af40d4f191addfa2614b
chifeichi <chifeichi@192.168.1.141>
2026-09-05 22:14:52 +0800

    fix(pd_disaggregation): refine setup and launch workflow

--- pd_disaggregation/install.sh
+++ pd_disaggregation/install.sh
@@ -7,12 +7,13 @@ apt-get install mpich libmpich-dev -y
 
 CANN_INSTALL_PATH=${CANN_INSTALL_PATH:-"/mnt/share/t00986241/b106"}
 source ${CANN_INSTALL_PATH}/ascend-toolkit/set_env.sh
-source ${CANN_INSTALL_PATH}/nnal/atb/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
 
 echo "1. install akernel and openyuanrong"
 pip install akernel_sdk-0.9.19-py3-none-any.whl
 
 echo "2. install vllm-ascend from source"
+pip uninstall -y vllm_ascend
 git clone -b releases/v0.23.0 https://github.com/vllm-project/vllm-ascend.git
 cd vllm-ascend && pip install -r requirements.txt --extra-index-url https://triton-ascend.osinfra.cn/pypi/simple/ --trusted-host triton-ascend.osinfra.cn
 export COMPILE_CUSTOM_KERNELS=1
@@ -38,20 +39,29 @@ export LD_LIBRARY_PATH=${CANN_INSTALL_PATH}/ascend-toolkit/latest/python/site-pa
 cd ../../
 
 echo "4.install verl"
+pip uninstall -y verl
 git clone https://github.com/verl-project/verl.git
 cd verl && git checkout v0.9.0
 pip install -r requirements-npu.txt --extra-index-url https://triton-ascend.osinfra.cn/pypi/simple/ --trusted-host triton-ascend.osinfra.cn
 pip install -v -e .
 cd ..
 
-echo "7.apply patch"
+echo "5.apply patch"
 cd vllm-ascend
 git apply --whitespace=nowarn ../verl-ascend-recipe/pd_disaggregation/patch/vllm-ascend.patch && cd ..
-
 cd verl
 git apply --whitespace=nowarn ../verl-ascend-recipe/pd_disaggregation/patch/verl.patch && cd ..
 
-echo "5.run uni-agent"
+echo "6.install bridge"
+git clone https://github.com/NVIDIA-NeMo/Megatron-Bridge.git
+cd Megatron-Bridge
+git checkout de93536e9
+export PYTHONPATH="$PWD/src:$PYTHONPATH"
+cd ..
+
+echo "7.run uni-agent"
+pip install swebench==4.1.0 && pip install mini-swe-agent==2.4.1 && pip install swe-rex==1.4.0
 git clone https://github.com/chifeichi/uni-agent.git
 cd uni-agent && git checkout cfc/new_release
+cp -f ../run_train_no_pd.sh examples/blackbox_recipes/claude_code/run_train_no_pd.sh
 bash examples/blackbox_recipes/claude_code/run_train_no_pd.sh
